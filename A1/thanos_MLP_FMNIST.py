f6cebb92422e7118362dfde35f21bbbd18d4d4ec
ThanosSkentzos <kalligerist@gmail.com>
2024-10-15 00:55:10 +0200

    thanos: add tuner

--- A1/thanos_MLP_FMNIST.py
+++ A1/thanos_MLP_FMNIST.py
@@ -14,8 +14,9 @@ from sklearn.preprocessing import StandardScaler
 # DATA
 fashion_mnist = k.datasets.fashion_mnist
 (X_train_full, y_train_full), (X_test, y_test) = fashion_mnist.load_data()
-X_train_full_scaled = (X_train_full - X_train_full.mean()) / X_train_full.std()
-
+m, s = X_train_full.mean(), X_train_full.std()
+X_train_full_scaled = (X_train_full - m) / s
+X_test = (X_test - m) / s  # should we scale with its own values
 X_train, X_val, y_train, y_val = train_test_split(
     X_train_full_scaled, y_train_full, test_size=0.1, random_state=42
 )
@@ -23,37 +24,64 @@ X_train, X_val, y_train, y_val = train_test_split(
 
 # %%
 # MODEL
-reg = {
-    "kernel_regularizer": k.regularizers.L2(0.1),
-    "kernel_initializer": k.initializers.RandomNormal(mean=0.0, stddev=0.05, seed=None),
-    "bias_initializer": "zeros",
-}
+import keras_tuner
 
 
-def NN():
+def NN(hp):
+    params = {
+        "kernel_regularizer": k.regularizers.L2(0.1),
+        "kernel_initializer": k.initializers.RandomNormal(
+            mean=0.0, stddev=0.05, seed=None
+        ),
+        "bias_initializer": "zeros",
+        "units": hp.Int("units", min_value=32, max_value=512, step=32),
+        "activation": hp.Choice("activation", ["relu", "tanh"]),
+    }
     model = k.models.Sequential()
     model.add(k.layers.Flatten(input_shape=[28, 28]))
-    model.add(k.layers.Dense(64, activation="relu", **reg))
+
+    model.add(k.layers.Dense(**params))
     # model.add(k.layers.Dropout(0.1))
-    model.add(k.layers.Dense(64, activation="relu", **reg))
+    model.add(k.layers.Dense(**params))
     # model.add(k.layers.Dropout(0.1))
-    model.add(k.layers.Dense(64, activation="relu", **reg))
+    model.add(k.layers.Dense(**params))
     # model.add(k.layers.Dropout(0.1))
     model.add(k.layers.Dense(10, activation="softmax"))
+
+    model.compile(
+        loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"]
+    )
     return model
 
 
-model = NN()
-model.compile(
-    loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"]
-)
+model = NN(keras_tuner.HyperParameters())
+
 model.summary()
 
+
 # %%
-# TRAIN
-history = model.fit(
-    X_train, y_train, batch_size=64, epochs=10, validation_data=(X_val, y_val)
+# TUNE
+
+tuner = keras_tuner.RandomSearch(
+    hypermodel=NN,
+    objective="val_accuracy",
+    max_trials=3,
+    executions_per_trial=2,
+    overwrite=True,
+    directory="models",
+    project_name="idl",
 )
+tuner.search_space_summary()
+# %%
+tuner.search(X_train, y_train, epochs=5, validation_data=(X_val, y_val))
+models = tuner.get_best_models(5)
+best_model = models[0]
+best_model.summary()
+
+# %%
+best_hps = tuner.get_best_hyperparameters(5)
+model = NN(best_hps[0])
+history = model.fit(X_train_full_scaled, y_train_full, batch_size=64, epochs=10)
 
 # %%
 # HISTORY
