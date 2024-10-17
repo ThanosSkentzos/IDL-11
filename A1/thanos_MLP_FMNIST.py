7ca2721980cd9b921878b79592480fa4dace222c
ThanosSkentzos <kalligerist@gmail.com>
2024-10-17 13:34:26 +0200

    thanos: different params for layers

--- A1/thanos_MLP_FMNIST.py
+++ A1/thanos_MLP_FMNIST.py
@@ -28,23 +28,37 @@ import keras_tuner
 
 
 def NN(hp):
-    params = {
-        "kernel_regularizer": k.regularizers.L2(0.1),
+    global_params = {
+        "kernel_regularizer": k.regularizers.L2(0.01),
         "kernel_initializer": k.initializers.RandomNormal(
             mean=0.0, stddev=0.05, seed=None
         ),
         "bias_initializer": "zeros",
-        "units": hp.Int("units", min_value=32, max_value=512, step=32),
-        "activation": hp.Choice("activation", ["relu", "tanh"]),
+        # "units": hp.Int("units", min_value=32, max_value=512, step=32),
+        # "activation": hp.Choice("activation", ["relu", "tanh"]),
+    }
+    l1_params = {**global_params,
+        "units": hp.Int("units1", min_value=256, max_value=512, step=32),
+        "activation": hp.Choice("activation1", ["relu", "tanh"]),
+    }
+
+    l2_params = {**global_params,
+        "units": hp.Int("units2", min_value=128, max_value=512, step=32),
+        "activation": hp.Choice("activation2", ["relu", "tanh"]),
+    }
+
+    l3_params = {**global_params,
+        "units": hp.Int("units3", min_value=64, max_value=256, step=32),
+        "activation": hp.Choice("activation3", ["relu", "tanh"]),
     }
     model = k.models.Sequential()
     model.add(k.layers.Flatten(input_shape=[28, 28]))
 
-    model.add(k.layers.Dense(**params))
+    model.add(k.layers.Dense(**l1_params))
     # model.add(k.layers.Dropout(0.1))
-    model.add(k.layers.Dense(**params))
+    model.add(k.layers.Dense(**l2_params))
     # model.add(k.layers.Dropout(0.1))
-    model.add(k.layers.Dense(**params))
+    model.add(k.layers.Dense(**l3_params))
     # model.add(k.layers.Dropout(0.1))
     model.add(k.layers.Dense(10, activation="softmax"))
 
@@ -65,15 +79,15 @@ model.summary()
 tuner = keras_tuner.RandomSearch(
     hypermodel=NN,
     objective="val_accuracy",
-    max_trials=3,
-    executions_per_trial=2,
+    max_trials=20,
+    executions_per_trial=1,
     overwrite=True,
     directory="models",
     project_name="idl",
 )
 tuner.search_space_summary()
 # %%
-tuner.search(X_train, y_train, epochs=5, validation_data=(X_val, y_val))
+tuner.search(X_train, y_train, epochs=10, validation_data=(X_val, y_val))
 models = tuner.get_best_models(5)
 best_model = models[0]
 best_model.summary()
