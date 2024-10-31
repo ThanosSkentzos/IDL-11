3d879329eae36bb2407de4f4751d13f4d2396a9d
J. Gireesan Mini <s3777103@u0065001.vuw.leidenuniv.nl>
2024-10-31 14:57:43 +0100

    Addded looped iteration.

--- A1/Jo_MLP_FMNIST.py
+++ A1/Jo_MLP_FMNIST.py
@@ -166,15 +166,138 @@ for each_dp1 in dropouts:
                 loss = history.history['loss']
                 val_loss = history.history['val_loss']
 
-                # Plot the loss
-                plt.figure(figsize=(10, 6))
-                plt.plot(loss, label='Training Loss')
-                plt.plot(val_loss, label='Validation Loss')
-                plt.xlabel('Epochs')
-                plt.ylabel('Loss')
-                plt.title(f'Model Loss over Epochs 3 Layers batch size of {each_size} for {oprimizer_str[i]} and {each_act}')
-                plt.legend()
-                plt.show()
-                plt.close()
-                
-                print(datetime.now() - startTime)
+            x_train, x_val, y_train, y_val = train_test_split(x_train_scaled, y_train, 
+                                                            test_size=0.1, random_state=42)
+
+            y_train_scaled = keras.utils.to_categorical(y_train, num_classes)
+            y_test_scaled = keras.utils.to_categorical(y_test, num_classes)
+            y_val_scaled = keras.utils.to_categorical(y_val, num_classes)
+            
+            print(str(optimizers[i]))
+            print(str(each_act))
+            model = keras.models.Sequential()
+            model.add(keras.layers.Flatten(input_shape=[28, 28]))
+
+            # model.add(keras.layers.Dropout(0.2))
+            model.add(keras.layers.Dense(64, activation="relu"))
+            # model.add(keras.layers.Dropout(0.2))
+            model.add(keras.layers.Dense(64, activation=each_act))
+            # model.add(keras.layers.Dropout(0.2))
+            model.add(keras.layers.Dense(64, activation=each_act))
+            model.add(keras.layers.Dense(10, activation='softmax'))
+            
+            early_stopping_cb = keras.callbacks.EarlyStopping(patience=10, 
+                                                              restore_best_weights=True)
+
+            # model.summary()
+
+            model.compile(loss='categorical_crossentropy',
+                    optimizer=optimizers[i],
+                    metrics=['accuracy'])
+
+            history = model.fit(x_train, y_train_scaled,
+                            batch_size=each_size,
+                            epochs=epochs,
+                            verbose=0,
+                            validation_data=(x_val, y_val_scaled),
+                            callbacks=[early_stopping_cb])
+
+            score = model.evaluate(x_test, y_test_scaled, verbose=0)
+            print('Test loss:', score[0])
+            print('Test accuracy:', score[1])
+
+            loss = history.history['loss']
+            val_loss = history.history['val_loss']
+
+            # Plot the loss
+            plt.figure(figsize=(10, 6))
+            plt.plot(loss, label='Training Loss')
+            plt.plot(val_loss, label='Validation Loss')
+            plt.xlabel('Epochs')
+            plt.ylabel('Loss')
+            plt.title('Model Loss over Epochs')
+            plt.legend()
+            plt.show()
+            plt.savefig("plot_image.png")  # Saves as a PNG image
+            
+            cell_name = get_cell_name(row_count, 10)
+            
+            sheet.append((3, "No", 3, each_size, str(each_act), oprimizer_str[i], score[0], score[1]))
+            img = Image("plot_image.png")
+            
+            img.width, img.height = 40, 40         # Resize image if necessary
+            sheet.add_image(img, cell_name)
+            row_count += 1
+
+workbook.save("sample_with_image.xlsx")
+
+
+# %%
+str(optimizers[0])
+
+# # %%
+# batch_size = 128
+# num_classes = 10
+# epochs = 12
+
+# # input image dimensions
+# img_rows, img_cols = 28, 28
+
+
+# if K.image_data_format() == 'channels_first':
+#     x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+#     x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+#     input_shape = (1, img_rows, img_cols)
+# else:
+#     x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+#     x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+#     input_shape = (img_rows, img_cols, 1)
+
+# # %%
+# x_train = x_train.astype('float32')
+# x_test = x_test.astype('float32')
+# x_train /= 255
+# x_test /= 255
+# print('x_train shape:', x_train.shape)
+# print(x_train.shape[0], 'train samples')
+# print(x_test.shape[0], 'test samples')
+
+# # convert class vectors to binary class matrices
+# y_train = keras.utils.to_categorical(y_train, num_classes)
+# y_test = keras.utils.to_categorical(y_test, num_classes)
+# print(y_train.shape)
+# print(y_test.shape)
+
+# model = Sequential()
+# model.add(Conv2D(32, kernel_size=(3, 3),
+#                  activation='relu',
+#                  input_shape=input_shape))
+# model.add(Conv2D(64, (3, 3), activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Dropout(0.25))
+# model.add(Flatten())
+# model.add(Dense(128, activation='relu'))
+# model.add(Dropout(0.5))
+# model.add(Dense(num_classes, activation='softmax'))
+
+# model.compile(loss=keras.losses.categorical_crossentropy,
+#               optimizer=keras.optimizers.Adadelta(),
+#               metrics=['accuracy'])
+
+# model.summary()
+
+# # %%
+# model.fit(x_train, y_train,
+#           batch_size=batch_size,
+#           epochs=epochs,
+#           verbose=1,
+#           validation_data=(x_test, y_test))
+
+# # %%
+# score = model.evaluate(x_test, y_test, verbose=0)
+# print('Test loss:', score[0])
+# print('Test accuracy:', score[1])
+
+
+
+# %%
