29029060024b274c2a595019a808e783075982da
J. Gireesan Mini <s3777103@vibranium.liacs.nl>
2024-11-05 10:19:08 -0500

    Regression Completed.

--- A1/jo_regression_cnn.py
+++ A1/jo_regression_cnn.py
@@ -16,7 +16,7 @@ labels = np.load("A1/data/labels.npy")
 # %%
 # Define the function to map (hour, minute) tuples to float values upto 
 def time_to_regression(row):
-    return row[0] + round(row[1]/60, 2)
+    return round((row[0] + row[1]/60.0), 4)
 
 img_rows, img_cols = 75, 75  #pixel size
 
@@ -26,8 +26,8 @@ new_labels = np.apply_along_axis(time_to_regression, axis=1, arr=labels)
 #%%
 images = images.astype('float32')/255.0
 
-x_train, x_test, y_train, y_test = train_test_split(images, new_labels, test_size=0.10, random_state=42) 
-x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.10, random_state=33) 
+x_train, x_test, y_train, y_test = train_test_split(images, new_labels, test_size=0.20, random_state=42) 
+x_test, x_val, y_test, y_val = train_test_split(x_test, y_test, test_size=0.50, random_state=33) 
 
 # %%
 batch_size = 64
@@ -45,57 +45,30 @@ else:
 print(x_val.shape)
 
 # %%
-
+# Custom loss function for common sense difference
 def reg_loss_funtion(y_true, y_pred):
     # Calculate the absolute error
     abs_diff = tf.abs(y_true - y_pred)
-    wrapped_diff = tf.abs(12 - abs_diff)
     
-    below_min_penalty = tf.reduce_sum(tf.square(tf.maximum(0.0, 0 - y_pred)))
-    above_max_penalty = tf.reduce_sum(tf.square(tf.maximum(0.0, y_pred - 12)))
-    
-    range_penalty = below_min_penalty + above_max_penalty
-
-    # Calculate the minimum error
-    min_error = tf.minimum(abs_diff, wrapped_diff)
+    min_error = tf.where((y_pred >= 0.) & (y_pred < 12.), tf.minimum(abs_diff, 12. - abs_diff), tf.abs(y_pred - y_true))
 
     # Return the mean squared of the minimum error
-    return tf.reduce_mean(tf.square(min_error) + 0.1 * range_penalty)
+    return tf.reduce_mean(tf.square(min_error))
 
-# def reg_custom_mae(y_true, y_pred):
-#     abs_diff = tf.abs(y_true - y_pred)
-#     wrapped_diff = tf.abs(12 - abs_diff)
-#     common_sense_error = tf.minimum(abs_diff, wrapped_diff)
-#     return common_sense_error
+def reg_custom_mae(y_true, y_pred):
+    abs_diff = tf.abs(y_true - y_pred)
+    wrapped_diff = tf.abs(12. - abs_diff)
+    common_sense_error = tf.minimum(abs_diff, wrapped_diff)
+    return tf.reduce_mean(common_sense_error, axis=0)
 
 
-# class CustomMAE(tf.keras.metrics.Metric):
-#     def __init__(self, name="custom_mae", **kwargs):
-#         super(CustomMAE, self).__init__(name=name, **kwargs)
-#         # Initialize variables to keep track of the total absolute error and count
-#         self.total_error = self.add_weight(name="total_error", initializer="zeros")
-#         self.count = self.add_weight(name="count", initializer="zeros")
-
-#     def update_state(self, y_true, y_pred, sample_weight=None):
-#         # Calculate the absolute difference between the true and predicted values
-
-#         abs_diff = tf.abs(y_true - y_pred)
-#         wrapped_diff = tf.abs(12 - abs_diff)
-#         common_sense_error = tf.minimum(abs_diff, wrapped_diff)        
-#         # Sum the absolute errors and update the total error
-#         self.total_error.assign_add(tf.reduce_sum(common_sense_error))
-        
-#         # Update the count with the batch size
-#         self.count.assign_add(tf.cast(tf.size(y_true), tf.float32))
-        
-#     def result(self):
-#         # Calculate the mean absolute error
-#         return self.total_error / self.count
-
-#     def reset_states(self):
-#         # Reset the metric's state (total error and count) between epochs
-#         self.total_error.assign(0.0)
-#         self.count.assign(0.0)
+def custom_huber_loss(y_true, y_pred, delta=1.0):
+    abs_diff = tf.abs(y_true - y_pred)
+    error = tf.where((y_pred >= 0.) & (y_pred < 12.), tf.minimum(abs_diff, 12. - abs_diff), tf.abs(y_pred - y_true))
+    is_small_error = tf.abs(error) <= delta
+    small_error_loss = 0.5 * tf.square(error)
+    large_error_loss = delta * (tf.abs(error) - 0.5 * delta)
+    return tf.reduce_mean(tf.where(is_small_error, small_error_loss, large_error_loss))
 
 
 # %%
@@ -128,15 +101,12 @@ def build_model(input_shape):
     model.add(keras.layers.Dropout(0.5))
     model.add(keras.layers.Dense(128, activation='relu'))
     model.add(keras.layers.Dropout(0.3))
-    
-    model.add(keras.layers.Dense(64, activation='relu'))
-    model.add(keras.layers.Dropout(0.1))
 
     model.add(keras.layers.Dense(1, activation='linear'))
     
     model.compile(optimizer=keras.optimizers.Adam(learning_rate=1e-3, 
                                                   weight_decay=1e-5), 
-                  loss=reg_loss_funtion, metrics=['mae'])
+                  loss=custom_huber_loss, metrics=[custom_huber_loss, 'mae'])
 
     return model
 
@@ -150,11 +120,11 @@ model.summary()
 # Initialize EarlyStopping with patience
 early_stopping = keras.callbacks.EarlyStopping(
     monitor='val_loss',  # metric to monitor
-    patience=40,          # number of epochs to wait for improvement
+    patience=100,          # number of epochs to wait for improvement
     restore_best_weights=True  # restore the best weights after stopping
 )
 reduce_lr = keras.callbacks.ReduceLROnPlateau(
-    monitor='val_loss', factor=0.5, patience=5, min_lr=1e-6
+    monitor='val_loss', factor=0.8, patience=10, min_lr=1e-6
 )
 
 
@@ -167,6 +137,9 @@ history = model.fit(x_train, y_train,
                 callbacks=[early_stopping, reduce_lr]
 )
 
+#%%
+# model.save_weights('75_regression_without_rlrop.weights.h5')
+
 
 # %%
 loss = history.history['loss']
@@ -178,19 +151,134 @@ plt.plot(loss, label='Training Loss')
 plt.plot(val_loss[:-1], label='Validation Loss')
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
-plt.title(f'{img_rows} x {img_cols} - Regression loss history with diminishing learning rate')
+plt.title(f'{img_rows} x {img_cols} - Regression loss history with diminishing learning rate and custom huber loss')
 plt.legend()
 plt.show()
 plt.close()
 
 # %%
-y_test_pred = model.predict(x_test)
+y_test_pred = model.predict(x_test).flatten()
 
 # %%
 abs_values = abs(y_test_pred.flatten()-y_test)
-common_sense_error = np.minimum(12-abs_values, abs_values)
+# common_sense_error = np.minimum(12-abs_values, abs_values)
+common_sense_error = np.where((y_test_pred.flatten() >= 0.) & (y_test_pred.flatten() < 12.), np.minimum(abs_values, 12. - abs_values), np.minimum(np.abs(y_test_pred.flatten() - y_test), np.abs(y_test_pred.flatten() - (12. - y_test))))
 
 score = np.mean(common_sense_error)
+score
+
+
+#%%
+plt.scatter(y_test_pred, y_test)
+plt.xlabel('Predicted')
+plt.ylabel('True')
+plt.title('Regression with diminishing learning rate with custom huber loss')
+# %%
+
+# y_true = np.array([0.1,1.2,2.3,3.4,4.5,5.6,6.7,7.8,8.9,10.11,11.22])
+# y_pred = np.array([-0.1,-1.2,2.3,3.4,4.5,5.6,6.7,-7.8,12.9,10.11,-11.22])
+
+# reg_loss_funtion(y_true, y_pred)
+# %%
+sorted_indices = np.argsort(y_test)
+sorted_y_test = y_test[sorted_indices]
+sorted_y_pred = y_test_pred[sorted_indices]
+
+same_indices = np.where(sorted_y_test == sorted_y_pred)[0]
+
+#%%
+# Plot y_true and y_pred
+plt.figure(figsize=(10, 6))
+plt.plot(sorted_y_test, label='y_true', marker='o', color='blue', linestyle='None')
+plt.plot(sorted_y_pred, label='y_pred', marker='o', color='orange', linestyle='None')
+
+# Highlight points where y_true and y_pred are the same
+plt.scatter(same_indices, sorted_y_test[same_indices], color='green', s=100, label='Matching Points')
+
+# Draw dotted lines between y_true and y_pred points at each index
+for i in range(len(sorted_y_test)):
+    plt.plot([i, i], [sorted_y_test[i], sorted_y_pred[i]], 'k:', alpha=0.6)  # 'k:' makes a black dotted line
+
+# Adding labels and legend
+plt.xlabel('Index')
+plt.ylabel('Values')
+plt.title('Plot of y_true and y_pred connected for with diminishing learning rate')
+plt.legend()
+
+# Show the plot
+plt.show()
+
+
+
 
 # %%
-score
+import matplotlib.pyplot as plt
+import numpy as np
+
+values = 1 - common_sense_error[sorted_indices]  # Random values for each angle
+
+# Create polar plot
+plt.figure(figsize=(6, 6))
+ax = plt.subplot(111, polar=True)
+ax.plot(2 * np.pi * sorted_y_test/12, values, marker='o')
+
+# Add title and show plot
+ax.set_title("Regression Common sense Error with Dim LR and custom huber loss")
+theta_ticks = np.linspace(0, 2 * np.pi, 12, endpoint=False)  # 12 hours around the clock
+ax.set_xticks(theta_ticks)
+
+ax.set_xticklabels(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'])
+
+plt.show()
+
+# %%
+
+# labels = np.array(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'])
+# # values = np.array([.5,1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5,9.5,10.5,11.5])  # Random values for each hour
+# values = np.arange(12)
+# vals = np.ones_like(values)
+# # Angles for each bar
+# angles = np.linspace(0, 2 * np.pi, len(labels), endpoint=False).tolist()
+# print(values)
+# # Plotting
+# fig, ax = plt.subplots(figsize=(8, 8), subplot_kw=dict(polar=True))
+# bars = ax.bar(values/12.0*2*np.pi , vals, color=plt.cm.viridis(values), alpha=0.7)
+
+# # Label each bar with hour labels
+# ax.set_xticks(angles)
+# ax.set_xticklabels(labels)
+
+# plt.show()
+# %%
+y_train_pred = model.predict(x_train.reshape(x_train.shape[0], img_rows, img_cols)).flatten()
+abs_values = abs(y_train_pred.flatten()-y_train)
+common_sense_error = np.minimum(abs_values, 12. - abs_values)
+cse_train = np.mean(common_sense_error)
+
+#%%
+y_val_pred = model.predict(x_val.reshape(x_val.shape[0], img_rows, img_cols)).flatten()
+abs_values = abs(y_val_pred.flatten()-y_val)
+common_sense_error = np.minimum(abs_values, 12. - abs_values)
+cse_val = np.mean(common_sense_error)
+
+#%%
+
+min(history.history['val_loss'])
+
+#%%
+min_val_loss_index = history.history['val_loss'].index(min(history.history['val_loss']))
+history.history['loss'][min_val_loss_index]
+# %%
+history.history['mae'][min_val_loss_index]
+
+#%%
+history.history['val_mae'][min_val_loss_index]
+
+#%%
+print(f"Min Val loss : {min(history.history['val_loss'])}")
+print(f"Loss at this : {history.history['loss'][min_val_loss_index]}")
+print(f"MAE : {history.history['mae'][min_val_loss_index]}")
+print(f"MAE Val : {history.history['val_mae'][min_val_loss_index]}")
+print(f"Common Sense MAE Train : {cse_train}")
+print(f"Common Sense MAE Val : {cse_val}")
+print(f"Common Sense MAE Test : {score}")
